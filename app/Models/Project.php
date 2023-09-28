6917551036abee90da3e56db995a717d8059820c
1shaker <salmazini@students.iugaza.edu.ps>
2023-09-28 23:32:20 +0300

    commit[3]

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Auth;
 
@@ -34,5 +35,10 @@ class Project extends Model
     {
         static::addGlobalScope('creator',static fn($builder)
         => $builder->where('creator_id', Auth::id()));
+    }
+
+    public function members():BelongsToMany
+    {
+        return $this->belongsToMany(User::class,Member::class);
     }
 }
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title></title></head>
+    <body>
+
+    </body>
+</html>
